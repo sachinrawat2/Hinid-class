3ad0e67f246b58e554fafcfa8c62a1978acfc0d0
sachinrawat2 <sachinrawat26379@gmail.com>
2026-02-17 18:53:08 +0530

    fixed script issue

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function enrollClick(btn) {
 
   // colorful button
@@ -21,27 +20,3 @@ function enrollClick(btn) {
     window.location.href = "contact.html";
   }, 1200);
 }
-=======
-function enrollClick(btn) {
-
-  // colorful button
-  btn.classList.add(
-    "bg-gradient-to-r",
-    "from-pink-500",
-    "via-orange-500",
-    "to-yellow-400",
-    "text-white",
-    "scale-95"
-  );
-
-  // popup show
-  const popup = document.getElementById("enrollPopup");
-  popup.classList.remove("opacity-0");
-  popup.classList.add("opacity-100");
-
-  // redirect
-  setTimeout(() => {
-    window.location.href = "contact.html";
-  }, 1200);
-}
->>>>>>> ceb548a48894a78bcf50215c8c1ce77842c2b467
